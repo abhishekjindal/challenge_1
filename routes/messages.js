5df0596269d98cd315fc5fe99980cede7447aeb5
abhishekjindal <abhishekjindal93@gmail.com>
2017-07-24 17:31:01 -0400

    init

---
+++ routes/messages.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var sha256 = require('sha256');
+var router = express.Router();
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('messages.db');
+
+
+
+/* GET home page. */
+router.get('/:hash', function(req, res, next) {
+	var hash = req.params.hash;
+	db.serialize(function(){
+  		db.all("SELECT * FROM messages WHERE hash='"+hash+"'", function(err, rows) {  
+  			if(err){
+  				console.log("error",err);
+  			} else {
+  				if (rows.length != 0) {
+  					res.send({"message": rows[0].message});
+  				} else {
+  					res.writeHead(404);
+  					res.end(JSON.stringify({"err_msg": "Message not found"}));
+  				}
+   			}
+  		});
+  	});
+});
+
+router.post('/', function(req, res, next) {
+  	var message = req.body.message;
+  	var digest = sha256(message);
+  	db.serialize(function(){
+  		db.all("SELECT * FROM messages WHERE hash='"+digest+"'", function(err, rows) {  
+  			if(err){
+  				console.log("error",err);
+  			} else {
+  				if (rows.length == 0) {
+  					db.run("INSERT INTO messages(hash,message) VALUES ('"+digest+"','"+message+"')");
+  				} else {
+  					console.log("hash already exists");
+  				}
+   			}
+  		});
+  	});
+  	res.send({"digest": digest});
+});
+
+
+module.exports = router;
